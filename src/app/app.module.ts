9da8dd1554761d7d6d228cf7efc3edd1cae2425f
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-07 22:27:39 +0200

    Add contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { localPhoneNumber } from '../validations/phone-number-validation';
+import { localIdNumber } from '../validations/id-validation';
+import { Contact } from '../models/contact';
+import { Title } from '../models/title-enum';
+import { Gender } from '../models/gender-enum';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+})
+export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
+  contact: Contact;
+  titles: Title[];
+  genders: Gender[];
+
+  constructor(private fb: FormBuilder) {
+    this.genders = [null, ...Object.values(Gender)] as Gender[];
+    this.titles = [null, ...Object.values(Title)] as Title[];
+  }
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', Validators.email],
+      id: ['', [Validators.required, Validators.maxLength(13)]],
+      title: [''],
+      phoneNumber: ['', [Validators.required, localPhoneNumber()]],
+    });
+  }
+
+  get controls() {
+    return this.contactForm.controls;
+  }
+
+  Save() {
+    alert(JSON.stringify(this.contactForm.value));
+  }
+}
