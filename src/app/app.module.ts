aeb946309b027a79100ddcd404f29a741965e902
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-07 23:33:04 +0200

    Add address component and zipcode directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ZipCodeDirective } from './directives/zipcode.directive';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContactComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule, ReactiveFormsModule
-  ],
+  declarations: [AppComponent, ContactComponent, ZipCodeDirective, AddressComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
