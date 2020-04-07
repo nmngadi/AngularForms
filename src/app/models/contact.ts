b9399f7325badffa26c083c15edf337d5baaaee8
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-07 09:07:14 +0200

    Add contact class

---
+++ src/app/models/contact.ts
@@ -0,0 +1,13 @@
+import { Gender } from './gender-enum';
+import { Title } from './title-enum';
+export class Contact {
+  firstName: string;
+  lastName: string;
+  gender: Gender;
+  IdNumber:string
+  birthDate: Date;
+  title: Title;
+  phoneNumber: string;
+  email: string;
+  poc:string;
+}
