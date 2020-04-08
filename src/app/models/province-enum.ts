a7ae097e8e2c3434ab722a7c7dc71a708dfd05d8
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-08 09:47:13 +0200

    Add Province enum

---
+++ src/app/models/province-enum.ts
@@ -0,0 +1,11 @@
+export enum Province {
+  kwazuluNatal = 'Kwazulu Natal',
+  Gauteng = 'Gauteng',
+  mpumalanga = 'Mpumalanga',
+  freestate = 'Free State',
+  northernCape= 'Northern Cape',
+  westernCape= 'Western Cape',
+  limpopo= 'Limpopo',
+  easternCape= 'Eastern Cape',
+  northWest= 'North West'
+}
