40527ec6d5f8a07e2111b22adc0a7b246bbc20c6
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-08 12:58:58 +0200

    Add textonly directive to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ZipCodeDirective } from './directives/zipcode.directive';
 import { AddressComponent } from './address/address.component';
 import { RouterModule } from '@angular/router';
+import { TextOnlyDirective } from './directives/text-only.directive';
 
 @NgModule({
-  declarations: [AppComponent, ContactComponent, ZipCodeDirective, AddressComponent],
+  declarations: [AppComponent, ContactComponent, ZipCodeDirective, AddressComponent, TextOnlyDirective],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, RouterModule.forRoot([
     { path: 'contact', component: ContactComponent },
     { path: 'address', component: AddressComponent },
