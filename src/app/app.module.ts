a6c93df5631c81070fa1d1ba4bfc4ac20e2044c1
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-04-08 09:45:53 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { ContactComponent } from './contact/contact.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ZipCodeDirective } from './directives/zipcode.directive';
 import { AddressComponent } from './address/address.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, ContactComponent, ZipCodeDirective, AddressComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, RouterModule.forRoot([
+    { path: 'contact', component: ContactComponent },
+    { path: 'address', component: AddressComponent },
+    ])],
   providers: [],
   bootstrap: [AppComponent],
 })
